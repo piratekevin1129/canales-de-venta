420da92b3d14f358d65114051881f4e654bc4031
Kevin Gaviria Fonnegra <desarrollo3@virtualcolors.com>
2023-06-16 03:20:21 -0500

    version 0.0.5

---
+++ scripts/tabs.js
@@ -0,0 +1,10 @@
+function clickHeaderTab(btn,t){
+    var tabs_header = btn.parentNode
+    var tabs_header_children = tabs_header.getElementsByClassName('cv-tab-header')
+    for(var i = 0;i<tabs_header_children.length;i++){
+        tabs_header_children[i].setAttribute('class','cv-tab-header')
+    }
+    tabs_header_children[t-1].classList.add('cv-tab-header-active')
+
+    document.getElementById('cv-tabs-container').className = 'cv-tabs-container-active-'+t
+}
